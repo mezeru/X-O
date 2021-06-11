884e36125b58a7d5bd61ce440ebe215857ffa226
Yash <57724250+mezeru@users.noreply.github.com>
2021-06-11 17:24:25 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,6 @@
 const WebSocket = require("ws")
-const server = new WebSocket.Server({port:3001})
+const Port = process.env.PORT || 3001
+const server = new WebSocket.Server({port:Port})
 
 const errorSend = (msg) =>{
     socket.send(msg);
